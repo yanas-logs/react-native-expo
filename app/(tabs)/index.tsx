c101bdfd74844855003f1cff6fa38e5c3244c336
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-20 05:33:19 +0000

    feat: implement offers list with FlatList and add offers data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { offers } from "../../data/offers";
+import { Item } from "../../type";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+  const limitedData = offers.slice(0, 7);
+
+  const renderItem = ({ item }: { item: Item }) => (
+    <View style={styles.card}>
+      <Text style={styles.cardTitle}>{item.title}</Text>
     </View>
   );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={limitedData}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffffff",
+  },
+  listContainer: {
+    padding: 16,
+  },
+  card: {
+    backgroundColor: "#1e1e1e",
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: "#333",
+    height: 200,
+    width: "100%",
+  },
+  cardTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
---
+++ data/offers.ts
@@ -0,0 +1,14 @@
+import { Item } from "../type";
+
+export const offers: Item[] = [
+    { id: "1", title: "Super" },
+    { id: "2", title: "Weekend Bonus" },
+    { id: "3", title: "Flash Sale Spesial" },
+    { id: "4", title: "End of Year Sale" },
+    { id: "5", title: "Today Promotion" },
+    { id: "6", title: "Voucher Sale 50%" },
+    { id: "7", title: "Cashback Spesial" },
+    { id: "8", title: "Weekly Mega Deals" },
+    { id: "9", title: "Free Delivery" },
+    { id: "10", title: "Reward Loyal Customer" },
+];
