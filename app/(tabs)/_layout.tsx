87377aaa92d124508d295018990a72aae41be4c4
yanas-logs <yanas.dev404@gmail.com>
2025-11-26 21:19:33 +0700

    chore(tabs): refactor _layout  tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,57 @@
-import { View, Text } from "react-native";
+import { Platform } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { useCartStore } from "../../store/cartStore";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const TabsLayout = () => {
   const cart = useCartStore((state) => state.cart);
   const cartItemCount = cart.reduce((total, item) => total + item.qty, 0);
+  const insets = useSafeAreaInsets();
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: "yellow",
         tabBarInactiveTintColor: "white",
+
         tabBarStyle: {
           backgroundColor: "#a00000ff",
-          paddingBottom: 6,
-          height: 60,
+          borderTopLeftRadius: 18,
+          borderTopRightRadius: 18,
+          paddingTop: 8,
+          paddingBottom: Platform.select({
+            ios: (insets.bottom || 0) + 8,
+            android: 12,
+            web: 8,
+          }),
+          paddingHorizontal: 8,
+          height: Platform.select({
+            ios: 70 + (insets.bottom || 0),
+            android: 75,
+            web: 70,
+          }),
         },
+
+        elevation: 10,
+
+        ...(Platform.OS === "web"
+          ? { boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.15)" }
+          : {
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: -4 },
+              shadowOpacity: 0.15,
+              shadowRadius: 10,
+            }),
+
         tabBarLabelStyle: {
           marginTop: 3,
           fontSize: 15,
+        },
+
+        tabBarItemStyle: {
+          paddingVertical: 6,
         },
       }}
     >
@@ -29,6 +60,7 @@ const TabsLayout = () => {
         options={{
           title: "Home",
           headerTitleAlign: "center",
+          headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="home" size={size} color={color} />
           ),
@@ -38,8 +70,9 @@ const TabsLayout = () => {
         name="search"
         options={{
           title: "Search",
+          headerShown: false,
           headerTitleAlign: "center",
-          
+
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="search" size={size} color={color} />
           ),
@@ -50,6 +83,7 @@ const TabsLayout = () => {
         options={{
           title: "Cart",
           headerTitleAlign: "center",
+          headerShown: false,
           tabBarBadge: cartItemCount > 0 ? cartItemCount : undefined,
           tabBarBadgeStyle: {
             backgroundColor: "#FF3B30",
@@ -68,6 +102,7 @@ const TabsLayout = () => {
         options={{
           title: "Profile",
           headerTitleAlign: "center",
+          headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="person-circle" size={size} color={color} />
           ),
