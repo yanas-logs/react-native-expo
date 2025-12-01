69de122f4054df3522954a884dae9e8dbdc526ca
yanas-logs <yanas.dev404@gmail.com>
2025-12-01 13:02:57 +0700

    chore: add initialize and auth state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useAuthStore } from "../store/authStore";
 
 export default function RootLayout() {
+  const initialize = useAuthStore((state) => state.initialize);
+
+  // Load auth state on app start
+  useEffect(() => {
+    initialize();
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(auth)"
+        options={{ headerShown: false, presentation: "modal" }}
+      />
     </Stack>
   );
 }
